6964cdf29b6039e6131a0a0320cec6b4b01d9a00
Petr Porobov <porobov@users.noreply.github.com>
2023-07-10 12:41:29 +0400

    Upala constants are loaded from npm now

--- src/app.config.js
+++ src/app.config.js
@@ -3,9 +3,5 @@ module.exports = {
      dev: `http://127.0.0.1:3001/`,
      production: ""
     },
-    // same for all chains for now
-    bundleId: {
-        discord: "0x00000000000000000000000000000000b85240ca14d7ec076483a7a30c9a70f4",
-    },
     poolAddress: "0x6D544390Eb535d61e196c87d6B9c80dCD8628Acd"
    };
